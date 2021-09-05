198b2ad4812175db2bf064256c43887f8653b598
abbas110r <19pa1a05f7@vishnu.edu.in>
2021-09-05 12:33:10 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,13 +1,54 @@
-import React,{Component} from "react";
-import ReactDOM from "react-dom";
-class App extends React.Component{
-  constructor(props){
-      super(props)
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+    this.increaseCount = this.increaseCount.bind(this);
+    this.decreaseCount = this.decreaseCount.bind(this);
   }
-  render(){
-    return(
-      <h1>Hello World {this.props.details.name}</h1>
+  increaseCount(){
+    this.setState(
+      function(prevState){
+          return(
+            {
+              count:prevState.count+1
+            }
+          )
+      }
     )
+  }
+  decreaseCount(){
+    this.setState(
+      function(prevState){
+        if(this.state.count<0){
+          return(
+            {
+              count:0
+            }
+          )
+        }
+        return(
+          {
+            count:prevState.count-1
+          }
+        )
+      }
+    )
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 id="output" style={{ marginBottom: '1rem' }} >{this.state.count}</h1>
+        <button style={{ marginRight: '1rem' }} onClick={this.increaseCount}>
+          +
+        </button>
+        <button onClick={this.decreaseCount}>-</button>
+      </div>
+    );
   }
 }
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-ReactDOM.render(<App details ={{ name:"Raza Abbas",age:10}}/>, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
